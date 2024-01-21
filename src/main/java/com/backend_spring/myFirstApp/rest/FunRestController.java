462b3750b5c20d6dd9030b5b2a6c5a6c8365da8d
Baiaman <baiamansama@gmail.com>
2024-01-21 18:25:18 +0900

    tweaking application.properties

--- src/main/java/com/backend_spring/myFirstApp/rest/FunRestController.java
+++ src/main/java/com/backend_spring/myFirstApp/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.backend_spring.myFirstApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "cartoon teamInfo"
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
 
     // here we expose '/' to return "Hello World!"
     @GetMapping("/")
