ad587355ea3653462cb8da4e49f69302afec2a9c
Baiaman <baiamansama@gmail.com>
2024-01-21 15:47:53 +0900

    first demo

---
+++ src/main/java/com/backend_spring/myFirstApp/MyFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend_spring.myFirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAppApplication.class, args);
+	}
+
+}
